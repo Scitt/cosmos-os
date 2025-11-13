bc8b3b28be09206225e05d7dc32354adbe0b4fb9
Scitt <54844178+Scitt@users.noreply.github.com>
2025-11-13 11:14:55 +0100

    Added -y option to mulvard-vpn

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,7 +18,7 @@ dnf5 -y install cosmic-desktop
 dnf5 -y copr disable ryanabx/cosmic-epoch
 # Mulvard vpn
 dnf5 config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
-dnf5 install mullvad-vpn
+dnf5 -y install mullvad-vpn
 
 #### Example for enabling a System Unit File
 
